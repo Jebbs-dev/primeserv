45c91e05828c9da5e9eab44ef27c2eff8be11452
Jebbs-dev <ojebiyifulness@gmail.com>
2024-08-15 23:01:32 +0100

    feat: add widgets data

---
+++ src/modules/infosection/data/widgets.ts
@@ -0,0 +1,23 @@
+import Facebook from "../../../../public/images/logos/facebook-white.png";
+import X from "../../../../public/images/logos/x.png";
+import LinkedIn from "../../../../public/images/logos/linkedIn.png";
+import Instagram from "../../../../public/images/logos/instagram.png";
+
+export const widgets = [
+  {
+    name: "LinkedIn",
+    icon: LinkedIn,
+    href: "https://www.linkedin.com/in/samuel-oyewusi-893668318?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+  },
+  {
+    name: "Instagram",
+    icon: Instagram,
+    href: "https://www.instagram.com/samueldmillionare?igsh=MWk1bHJubmEwZmo4MQ==",
+  },
+  {
+    name: "Facebook",
+    icon: Facebook,
+    href: "https://www.facebook.com/share/TD2zHDhGapc8wJAu/?mibextid=LQQJ4d",
+  },
+  { name: "X", icon: X, href: "" },
+];
