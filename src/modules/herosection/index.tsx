e092db4f13ec8dcdc140a1f32b5d066f938b6661
Jebbs-dev <ojebiyifulness@gmail.com>
2024-08-19 16:44:30 +0100

    adjust width and spacing for screen-sizes

--- src/modules/herosection/index.tsx
+++ src/modules/herosection/index.tsx
@@ -6,15 +6,15 @@ interface HeroSectionProps {}
 export const HeroSection: FunctionComponent<HeroSectionProps> = () => {
   return (
     <div>
-      <div className="w-full h-[100vh] md:h-[900px] lg:h-[850px] bg-[url('/images/second-bg.jpg')] bg-cover bg-black bg-opacity-50">
-        <div className="py-10 md:py-24">
+      <div className="w-full h-[100vh] iphone:h-[650px] md:h-[900px] lg:h-[850px] bg-[url('/images/second-bg.jpg')] bg-cover bg-black bg-opacity-50 md:mb-20">
+        <div className="py-10 md:py-24 flex justify-center md:justify-normal">
           <img
             src="/serv.png"
             alt="logo"
-            className="ml-10 md:ml-24 w-32 md:w-56"
+            className="md:ml-24 w-32 md:w-56"
           />
         </div>
-        <div className="mx-auto lg:ml-56 w-72 md:w-[70%] lg:w-full max-w-3xl pb-24 md:pb-32 lg:pb-56">
+        <div className="mx-auto lg:ml-56 w-[350px] md:w-[70%] lg:w-full max-w-3xl lg:pb-56">
           <div className="">
             <h1 className="text-3xl font-bold text-black md:text-6xl">
               We Help Cosmetic Dentist Clinics Achieve Outstanding ROI Through
